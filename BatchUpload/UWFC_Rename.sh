92267a188ba7624bd4d6024246663746e483e8fd
jmhuie <52302862+jmhuie@users.noreply.github.com>
2019-10-08 16:07:48 -0700

    made rename more universal

--- BatchUpload/UWFC_Rename.sh
+++ BatchUpload/UWFC_Rename.sh
@@ -30,11 +30,11 @@ do
     fi
     echo $line #tells you which file it's workig on
     unzip -q ./zips/$line -d ./unzips/ #unzips VNHM download into Unzips folder
-    if test -f ./unzips/Info.txt # looks for Info file from VNHM
+    if test -f ./unzips/*.txt # looks for Info file from VNHM
     then 
         #dos2unix ./unzips/Info.txt
-        Sorce=$(awk '/^Sorce: / {print $2}' ./unzips/Info.txt) #looks in Info file for Museum and CatNum
-        CatNum=$(awk '/^SorceID: / {print $2}' ./unzips/Info.txt)
+        Sorce=$(awk '/^Sorce: / {print $2}' ./unzips/*.txt) #looks in Info file for Museum and CatNum
+        CatNum=$(awk '/^SorceID:/ {print $2}' ./unzips/*.txt)
         Source=${Sorce%$'\r'} #strips carriage returns so values can be set as variables
         Number=${CatNum%$'\r'}	
         echo $Source #prints out museum and cat num
@@ -52,7 +52,6 @@ do
         echo "Not UWFC" # tells you if wrong museum
     fi
     name="${Museum}_${Collection}_${Number}_body" #takes all variables to set name
-    jpgname="${name}_" #adds an underscore so when jpgs get renamed they can have a trailing number 
     if test -f ./ToUpload/$name.log
     then
         output=$(ls ./ToUpload/$name*.log | wc -l)
@@ -64,12 +63,15 @@ do
             echo $name
         fi
     fi
+    echo $name
     mv ./unzips/*.log ./ToUpload/"${name}.log" #moves the log file from unzips to ToUpload and renames it with name variable
     mv ./unzips/*.txt ./ToUpload/"${name}.txt"    
-	unzip -q ./unzips/Stack.zip -d ./unzips/"${name}" #unzips the Stack of images into a folder with the proper name
-    for f in ./unzips/"${name}"/*.jpg ; do mv $f ${f//Image/$jpgname} ; done #renames each jpg in the stack of images
+	unzip -q ./unzips/*.zip -d ./unzips/"${name}" #unzips the Stack of images into a folder with the proper name
     cd ./unzips/"${name}"
-    zip -q -r ./"${name}.zip" *
+    filetype="tif"
+    num=0
+    for i in * ; do mv "$i" "${name}_$(printf '%04d' $num).${filetype}"; ((num++)); done #renames each file
+    zip -q -r ./"${name}.zip" . -i *
     cd ../.. 
     mv ./unzips/"${name}"/"${name}.zip" ToUpload/
     rm -r unzips/ #removes unzip folder
